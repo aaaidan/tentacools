10fc42b6e573414d5caf0266a23dcd0421269deb
Aidan Fraser <aaaidan@gmail.com>
2017-01-21 12:09:04 +1300

    noodle appendages

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
+/// <reference path="../tsd/phaser.d.ts"/>
+
 const maxForce = 2000; // who knows
 const SHOW_PHYSICS_DEBUG = false;
+const gui = new dat.GUI();
 
 class Arm {
 
 	sprite: Phaser.Group;
 	balls: Phaser.Sprite[];
 	game: Phaser.Game;
+	tip: Phaser.Sprite;
+	springs: any[];
 
 	constructor(game:Phaser.Game, spriteName:String) {
 		this.game = game;
@@ -21,6 +26,7 @@ class Arm {
 			this.balls.push( ball );
 		}
 		this.game.physics.p2.enable( this.balls, SHOW_PHYSICS_DEBUG );
+		this.tip = this.balls[this.balls.length-1];
 
 		var lastBall:Phaser.Sprite = null;
 		this.balls.forEach( b => {
@@ -28,7 +34,8 @@ class Arm {
 			b.body.collideWorldBounds = false;			
 			if (lastBall) {
 				this.game.physics.p2.createRevoluteConstraint( b, [0,0], lastBall, [0,20], maxForce );
-				this.game.physics.p2.createRotationalSpring( b, lastBall, 0, 80, 4 );
+				var spring = this.game.physics.p2.createRotationalSpring( b, lastBall, 0, 80, 15 );
+				this.springs.push(spring);
 			}
 			lastBall = b;
 		});
@@ -46,13 +53,9 @@ class Arm {
 	}
 }
 
-/// <reference path="../tsd/phaser.d.ts"/>
 class SimpleGame {
     game: Phaser.Game;
     mouth: Phaser.Sprite;
-    arm1: Phaser.Sprite;
-	arm2: Phaser.Sprite;
-	arm3: Phaser.Sprite;
 
 	cursors: Phaser.CursorKeys;
 	cursors2: Phaser.CursorKeys;
@@ -63,6 +66,8 @@ class SimpleGame {
 	j3: Phaser.Physics.P2.RevoluteConstraint;
 
 	a1: Arm;
+	a2: Arm;
+	a3: Arm;
 
     constructor() {
         this.game = new Phaser.Game(640, 480, Phaser.AUTO, 'content', {
@@ -70,30 +75,15 @@ class SimpleGame {
         });
     }
     preload() {
-        this.game.load.image("decepticon", "assets/square.jpg");
+		// this.game.load.image("ball1", "assets/ball-1.png");
+		// this.game.load.image("ball2", "assets/ball-2.png");
+		// this.game.load.image("ball3", "assets/ball-3.png");
 
-		this.game.load.image("ball1", "assets/ball-1.png");
-		this.game.load.image("ball2", "assets/ball-2.png");
-		this.game.load.image("ball3", "assets/ball-3.png");
-
-		this.game.load.image("arm1", "assets/tentacool-01.png");
-		this.game.load.image("arm2", "assets/tentacool-02.png");
-		this.game.load.image("arm3", "assets/tentacool-03.png");
-		this.game.load.image("mouth", "assets/mouth.png");
+		// this.game.load.image("mouth", "assets/mouth.png");
     }
-
-	forEachArm( cb: (a:Phaser.Sprite) => void ) {
-		cb(this.arm1);
-		cb(this.arm2);
-		cb(this.arm3);
-	}
 
     create() {
         this.mouth = this.game.add.sprite(this.game.width/2, this.game.height/2, "mouth");
-        this.arm1 = this.game.add.sprite(this.game.width/2, this.game.height * 0.1, "arm1");
-		this.arm2 = this.game.add.sprite(this.game.width * 0.1, this.game.height * 0.8, "arm2");
-		this.arm3 = this.game.add.sprite(this.game.width * 0.9, this.game.height * 0.8, "arm3");
-
 		this.mouth.bringToTop();
 
 		// this.mouth.scale.set(0.1);
@@ -103,44 +93,11 @@ class SimpleGame {
 
         this.game.physics.startSystem(Phaser.Physics.P2JS);
 
-        // Enabled physics on our sprites
-        this.game.physics.p2.enable([this.mouth, this.arm1, this.arm2, this.arm3], SHOW_PHYSICS_DEBUG);
+        // Enabled physics on mouth
+        this.game.physics.p2.enable([this.mouth], SHOW_PHYSICS_DEBUG);
 
 		// setup behaviour of individual bits
 		this.mouth.body.mass = 5;
-		this.forEachArm( a => {
-			a.body.collideWorldBounds = false;
-			// a.body.clearShapes(); // remove the default rectangle
-		});
-
-        // Make our one body motionless
-        // this.mouth.body.static = true;
-        
-        // Now create a sprite between our two bodies, parameters are rest length, stiffness and damping
-        // Rest length is the length of the spring at rest ( where it's not under pressure )
-        // Stiffness is the resistance to movement of the spring
-        // Damping determines how fast the spring loses it's "boing"  Our low damping keeps our spring "boinging"
-        // Boing is a word I made up to describe the up and down motion of a spring doing it's spring thing
-        // var sp1 = this.game.physics.p2.createSpring(this.mouth, this.arm1, 100, 5, 0.2);
-		// var sp2 = this.game.physics.p2.createSpring(this.mouth, this.arm2, 100, 5, 0.2);
-		// var sp3 = this.game.physics.p2.createSpring(this.mouth, this.arm3, 100, 5, 0.2);
-
-		var setupRevoluteConstraints = () => {
-			this.j1 = this.game.physics.p2.createRevoluteConstraint( this.mouth, [0,0], this.arm1, [0,150], maxForce );
-			this.j2 = this.game.physics.p2.createRevoluteConstraint( this.mouth, [0,0], this.arm2, [0,150], maxForce );
-			this.j3 = this.game.physics.p2.createRevoluteConstraint( this.mouth, [0,0], this.arm3, [0,150], maxForce );
-
-			this.j1.setLimits(
-				2 * Math.PI / 3 * 0 - 0.2,
-				2 * Math.PI / 3 * 0 + 0.2);
-			this.j2.setLimits(
-				2 * Math.PI / 3 * 1 - 0.2,
-				2 * Math.PI / 3 * 1 + 0.2);
-			this.j3.setLimits(
-				2 * Math.PI / 3 * 2 - 0.2,
-				2 * Math.PI / 3 * 2 + 0.2);
-		}
-		setupRevoluteConstraints();
 		
 		var setupCursors = () => {
 			this.cursors = this.game.input.keyboard.createCursorKeys();
@@ -159,17 +116,18 @@ class SimpleGame {
 		}
 		setupCursors();
 
-		var setupNoodlyAppendage = (imageName, rotation) => {
-			this.a1 = new Arm(this.game, imageName);
-			// this.a1.sprite.position.set( 100,100 );
-			this.game.world.add(this.a1.sprite);
-			this.a1.attachTo(this.mouth.body, rotation);
+		var createNoodlyAppendage = (imageName, rotation) => {
+			var arm = new Arm(this.game, imageName);
+			this.game.world.add(arm.sprite);
+			arm.attachTo(this.mouth.body, rotation);
+			return arm;
 		}
-		setupNoodlyAppendage("ball1", 2*Math.PI * (0/3) );
-		setupNoodlyAppendage("ball2", 2*Math.PI * (1/3) );
-		setupNoodlyAppendage("ball3", 2*Math.PI * (2/3) );
+		this.a1 = createNoodlyAppendage("ball1", 2*Math.PI * (0/3) );
+		this.a2 = createNoodlyAppendage("ball2", 2*Math.PI * (1/3) );
+		this.a3 = createNoodlyAppendage("ball3", 2*Math.PI * (2/3) );
 		
-		
+		this.mouth.body.rotateRight(3000); // temp hack, counter inertial twisting of initialisation of appendages
+
 		window["game"] = this;
     }
 
@@ -200,9 +158,9 @@ class SimpleGame {
 			
 		} 
 
-		forceBody(this.arm1, this.cursors, FORCE);
-		forceBody(this.arm2, this.cursors2, FORCE);
-		forceBody(this.arm3, this.cursors3, FORCE);
+		forceBody(this.a1.tip, this.cursors, FORCE);
+		forceBody(this.a2.tip, this.cursors2, FORCE);
+		forceBody(this.a3.tip, this.cursors3, FORCE);
 
 	}
 }
