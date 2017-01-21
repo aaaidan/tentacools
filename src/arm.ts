c83ee197206b270573f78ef17cf5b9228a98f9e7
ajackson <Azzamatronic@Aaron-Jacksons-MacBook.local>
2017-01-21 17:03:49 +1300

    Split out arm to own file. Added Food.ts

---
+++ src/arm.ts
@@ -0,0 +1,88 @@
+class Arm {
+
+	static armIdCounter: number = 0;
+
+	id: number;
+	sprite: Phaser.Group;
+	balls: Phaser.Sprite[];
+	game: Phaser.Game;
+	tip: Phaser.Sprite;
+	springs: any[];
+	hinges: Phaser.Physics.P2.RevoluteConstraint[];
+
+
+	constructor(game: Phaser.Game, spriteName: String, armNumber: number, total: number) {
+		this.game = game;
+		this.balls = [];
+		this.springs = [];
+		this.hinges = [];
+		this.sprite = new Phaser.Group(this.game);
+
+		const angle = Math.PI * 2 * (armNumber / total);
+		const segmentLength = 10;
+		const totalMass = 1;
+		const segmentCount = 15;
+
+		for (var i = 0; i < segmentCount; i++) {
+			let x = Math.cos(angle) * i * segmentLength + game.world.centerX;
+			let y = Math.sin(angle) * i * segmentLength + game.world.centerY;
+			var ball: Phaser.Sprite = this.game.add.sprite(x, y, "segment");
+			ball.tint = armNumber / total * 0x00ffff + 1 /armNumber / total * 0xff0000;
+			ball.scale.set(1 / (1 + i / (segmentCount - 1)));
+			this.balls.push(ball);
+		}
+		this.game.physics.p2.enable(this.balls, SHOW_PHYSICS_DEBUG);
+		this.tip = this.balls[this.balls.length - 1];
+
+		var lastBall: Phaser.Sprite = null;
+		this.balls.forEach(b => {
+			b.body.mass = totalMass / segmentCount;
+			b.body.collideWorldBounds = false;
+
+		if (lastBall) {
+				var hinge = this.game.physics.p2.createRevoluteConstraint(b, [0, 0], lastBall, [0, 20], maxForce);
+				// hinge.setStiffness(armLengthStiffness);
+				// hinge.setRelaxation(armLengthRelaxation);
+				this.hinges.push(hinge);
+
+				var spring = this.game.physics.p2.createRotationalSpring(b, lastBall, 0, tweaks.stiffness, tweaks.damping);
+				this.springs.push(spring);
+			}
+			lastBall = b;
+		});
+
+		stiffness.addListener((value) => {
+			this.springs.forEach(s => {
+				s.data.stiffness = value;
+				console.log("stiff", value, this.id);
+			});
+		});
+		damping.addListener((value) => {
+			this.springs.forEach(s => {
+				s.data.damping = value;
+				console.log("damp", value, this.id);
+			});
+		});
+		armLengthStiffness.addListener(value => {
+			this.hinges.forEach(h => {
+				h.setStiffness(value);
+			})
+		});
+		armLengthRelaxation.addListener(value => {
+			this.hinges.forEach(h => {
+				h.setRelaxation(value);
+			})
+		});
+	}
+
+	getBase() {
+		return this.balls[0].body;
+	}
+
+	attachTo(body: Phaser.Physics.P2.Body, rotation: number) {
+		this.game.physics.p2.createRevoluteConstraint(body, [0, 0], this.getBase(), [0, 0], maxForce);
+		const USELESS = 0; // setting rest rotation in constructor doesn't work properly for some mysterious reason
+		var rotationSpring = this.game.physics.p2.createRotationalSpring(body, this.getBase(), USELESS, 120, 5);
+		rotationSpring.data.restAngle = rotation;
+	}
+}
---
+++ src/food.ts
@@ -0,0 +1,27 @@
+class Food {
+
+    constructor (game: Phaser.Game, spriteName: String, collisionGroups: Phaser.Physics.P2.CollisionGroup[]) {
+
+    // var food = allFood.create(this.game.world.randomX, this.game.world.randomY, 'food');
+	// food.body.setRectangle(40, 40);
+	// food.body.setCollisionGroup(foodCollisionGroup);
+
+	// food.body.collides([foodCollisionGroup, starfishCollisionGroup]);
+    }
+
+
+}
+
+
+class FoodGroup {
+
+    group: Phaser.Group;
+    collisionGroup: Phaser.Physics.P2.CollisionGroup;
+
+    constructor(game: Phaser.Game) {
+        this.group = game.add.group();
+		this.group.enableBody = true;
+		this.group.physicsBodyType = Phaser.Physics.P2JS;
+    }
+
+}
